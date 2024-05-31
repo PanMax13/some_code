a5e24f70c2946b296892028a8daa117056a44d9f
PanMax13 <panezhamax@gmail.com>
2024-05-31 22:13:24 +0300

    Initial Commit

---
+++ sfhhjkasfd/sfhhjkasfdApp.swift
@@ -0,0 +1,17 @@
+//
+//  sfhhjkasfdApp.swift
+//  sfhhjkasfd
+//
+//  Created by Максим Панежа on 31.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct sfhhjkasfdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
